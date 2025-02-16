87e985d25f2bee68a8533ea0ea217ae4904c2290
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-16 10:27:16 +0530

    update types

--- src/data/data.ts
+++ src/data/data.ts
@@ -24,10 +24,13 @@ export type TTableColumnEntry = {
     readonly column_is_key: string;
 }
 
-export type TColumnStructure = {
+export type TColumnStructureHead = {
     column_name : string,
     column_type : string,
     column_is_key : boolean;
+}
+
+export type TColumnStructureMeta = TColumnStructureHead & {
     column_data_preview : TDataPreviewTuple
     column_min : number
     column_max : number
@@ -35,11 +38,11 @@ export type TColumnStructure = {
     column_mean : number
 }
 
-export type TColumnDataSeries = TColumnStructure & {
+export type TColumnStructureData = TColumnStructureMeta & {
     column_data : TDataArray
 }
 
 export type TTableStructure = {
     table_name: string;
-    table_columns: readonly TColumnStructure[];
+    table_columns: readonly TColumnStructureHead[];
 }
